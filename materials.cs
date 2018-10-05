5af570826cc847ae9b6ee59d6dd00156f1769530
Tyler Russ <tylerjruss27@gmail.com>
2018-10-04 20:20:45 -0600

    Initial commit

---
+++ materials.cs
@@ -0,0 +1,28 @@
+/*using System;
+using System.IO;
+
+namespace myApp
+{
+    class materials
+    {
+      static void Main()
+    {
+        // Read each line of the file into a string array. Each element
+        // of the array is one line of the file.
+        string[] lines = System.IO.File.ReadAllLines("materials.txt");
+
+        // Display the file contents by using a foreach loop.
+        foreach (string line in lines)
+        {
+        // Print Contents of the file
+            Console.WriteLine(line);
+        }
+
+        // Keep the console window open in debug mode.
+        Console.WriteLine("Press any key to exit.");
+        System.Console.ReadKey();
+    }
+           
+        }
+    }
+*/
