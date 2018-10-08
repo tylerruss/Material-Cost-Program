aea325e4d179a1e8ca5d3f68190a4739f79a03dd
Tyler Russ <tylerjruss27@gmail.com>
2018-10-08 11:24:38 -0600

    Initial

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -7,19 +7,11 @@ namespace myApp
     {
       static void Main(String[] args)
     {
-        double cost;
-        Console.WriteLine("Please enter the door's width in inches: ");
-       double width = double.Parse(Console.ReadLine());
+       String material;
+       double length;
+       double width;
+       double cost;
 
-        Console.WriteLine("Please enter the door's height in inches: ");
-       double height = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Please enter the door's price per sq feet: ");
-        double price = double.Parse(Console.ReadLine());
-
-        cost = width * height * price;
-
-        Console.WriteLine("The price of the door will be: $" + cost);
         }
     }
 }
