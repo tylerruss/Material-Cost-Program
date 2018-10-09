027bf8ff23a9ac6dca23f31c6c302d5d134bec47
Tyler Russ <tylerjruss27@gmail.com>
2018-10-08 20:05:23 -0600

    Update

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -9,6 +9,7 @@ namespace myApp
     {
       static void Main(String[] args)
     {
+        //create dictionary of materials and their price per square foot
 
         Dictionary<string, int> dict = new Dictionary<string, int>()
          {
@@ -24,12 +25,14 @@ namespace myApp
              
          };
 
-
+        //declare variables
        String material;
        double length;
        double width;
        double area;
 
+
+        //Display material types and request input for material, width, and length
         Console.WriteLine();
         Console.WriteLine("Materials: carpet, wood, tile, pvc, concrete, brick, stone, granite, marble");
         Console.WriteLine();
@@ -47,22 +50,19 @@ namespace myApp
 
             area = length * width;
 
-
-            
-           
-
             //returns price of material
             //returns area
             //returns material's price per square foot * length * width 
+            //decimals are rounded to 2 places
 
            if (material == "carpet")
            
            {
                Console.WriteLine();
                Console.WriteLine("Price of carpet: $" + dict["carpet"]  + " per square foot");
-               Console.WriteLine("Total Area = " + area + " sq ft");
-               Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + dict["carpet"] * area);
-               Console.WriteLine("Total Cost w/ installation = $" + area * dict["carpet"] * 1.8);
+               Console.WriteLine("Total Area = " + Math.Round(area, 2) + " sq ft");
+               Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + Math.Round(dict["carpet"] * area, 2));
+               Console.WriteLine("Total Cost w/ installation = $" + Math.Round(area * dict["carpet"] * 1.8, 2));
                Console.WriteLine();
            }
 
@@ -71,9 +71,9 @@ namespace myApp
          {
                 Console.WriteLine();
                 Console.WriteLine("Price of wood: $" + dict["wood"] + " per square foot");
-                Console.WriteLine("Total Area = " + area + " sq ft");
-                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + dict["wood"] * area);
-                Console.WriteLine("Total Cost w/ installation = $" + area * dict["wood"] * 1.8);
+                Console.WriteLine("Total Area = " + Math.Round(area, 2) + " sq ft");
+                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + Math.Round(dict["wood"] * area, 2));
+                Console.WriteLine("Total Cost w/ installation = $" + Math.Round(area * dict["wood"] * 1.8, 2));
                 Console.WriteLine();
          }
 
@@ -82,9 +82,9 @@ namespace myApp
          {
                 Console.WriteLine();
                 Console.WriteLine("Price of tile: $" + dict["tile"] + " per square foot");
-                Console.WriteLine("Total Area = " + area + " sq ft");
-                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + dict["tile"] * area);
-                Console.WriteLine("Total Cost w/ installation = $" + area * dict["tile"] * 1.8);
+                Console.WriteLine("Total Area = " + Math.Round(area, 2) + " sq ft");
+                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + Math.Round(dict["tile"] * area, 2));
+                Console.WriteLine("Total Cost w/ installation = $" + Math.Round(area * dict["tile"] * 1.8, 2));
                 Console.WriteLine();
          }
          
@@ -92,9 +92,9 @@ namespace myApp
          {
                 Console.WriteLine();
                 Console.WriteLine("Price of pvc: $" + dict["pvc"] + " per square foot");
-                Console.WriteLine("Total Area = " + area + " sq ft");
-                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + dict["pvc"] * area);
-                Console.WriteLine("Total Cost w/ installation = $" + area * dict["pvc"] * 1.8);
+                Console.WriteLine("Total Area = " + Math.Round(area, 2) + " sq ft");
+                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + Math.Round(dict["pvc"] * area, 2));
+                Console.WriteLine("Total Cost w/ installation = $" + Math.Round(area * dict["pvc"] * 1.8, 2));
                 Console.WriteLine();
          }
 
@@ -102,9 +102,9 @@ namespace myApp
          {
                 Console.WriteLine();
                 Console.WriteLine("Price of brick: $" + dict["brick"] + " per square foot");
-                Console.WriteLine("Total Area = " + area + " sq ft");
-                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + dict["brick"] * area);
-                Console.WriteLine("Total Cost w/ installation = $" + area * dict["brick"] * 1.8);
+                Console.WriteLine("Total Area = " + Math.Round(area, 2) + " sq ft");
+                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + Math.Round(dict["brick"] * area, 2));
+                Console.WriteLine("Total Cost w/ installation = $" + Math.Round(area * dict["brick"] * 1.8, 2));
                 Console.WriteLine();
          }
 
@@ -112,9 +112,9 @@ namespace myApp
          {
                 Console.WriteLine();
                 Console.WriteLine("Price of stone: $" + dict["stone"] + " per square foot");
-                Console.WriteLine("Total Area = " + area + " sq ft");
-                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + dict["stone"] * area);
-                Console.WriteLine("Total Cost w/ installation = $" + area * dict["stone"] * 1.8);
+                Console.WriteLine("Total Area = " + Math.Round(area, 2) + " sq ft");
+                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + Math.Round(dict["stone"] * area, 2));
+                Console.WriteLine("Total Cost w/ installation = $" + Math.Round(area * dict["stone"] * 1.8, 2));
                 Console.WriteLine();
          }
 
@@ -122,9 +122,9 @@ namespace myApp
          {
                 Console.WriteLine();
                 Console.WriteLine("Price of granite: $" + dict["granite"] + " per square foot");
-                Console.WriteLine("Total Area = " + area + " sq ft");
-                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + dict["granite"] * area);
-                Console.WriteLine("Total Cost w/ installation = $" + area * dict["granite"] * 1.8);
+                Console.WriteLine("Total Area = " + Math.Round(area, 2) + " sq ft");
+                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + Math.Round(dict["granite"] * area, 2));
+                Console.WriteLine("Total Cost w/ installation = $" + Math.Round(area * dict["granite"] * 1.8, 2));
                 Console.WriteLine();
          }
 
@@ -132,15 +132,15 @@ namespace myApp
          {
                 Console.WriteLine();
                 Console.WriteLine("Price of marble: $" + dict["marble"] + " per square foot");
-                Console.WriteLine("Total Area = " + area + " sq ft");
-                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + dict["marble"] * area);
-                Console.WriteLine("Total Cost w/ installation = $" + area * dict["marble"] * 1.8);
+                Console.WriteLine("Total Area = " + Math.Round(area, 2) + " sq ft");
+                Console.WriteLine("Material Cost of " + width + "x" + length + " = $" + Math.Round(dict["marble"] * area, 2));
+                Console.WriteLine("Total Cost w/ installation = $" + Math.Round(area * dict["marble"] * 1.8, 2));
                 Console.WriteLine();
          }
 
          else {
 
-             Console.WriteLine("Invalid material");
+             Console.WriteLine("Misspelled or invalid material");
          }
       }
     }
